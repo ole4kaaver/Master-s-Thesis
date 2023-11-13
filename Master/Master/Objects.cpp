1b032628924a363b29e8e33f1391ef4c4ef6d3a5
Оля Аверкина <olga.averkina.01@mail.ru>
2023-11-13 14:09:04 +0700

    'test3'

--- Master/Master/Objects.cpp
+++ Master/Master/Objects.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <list>
 using namespace std;
 
 class Component
@@ -19,4 +20,34 @@ public:
 	double multiplierToPhasePermeability;
 	double viscosity;
 	double density;
+	list <Component> components;
+};
+
+class FiniteElement : Phase
+{
+public:
+	int number;
+	double xBegin, xEnd;
+	list <Phase> phases;
+};
+
+class Init : FiniteElement
+{
+public:
+	double gridBegin, gridEnd, dischargeRatio;
+	int numberOfPartitions;
+	double tBegin, tEnd, dt;
+
+	void ReadingGrid(string grid)
+	{
+		ifstream fGrid;
+		fGrid.open(grid + ".txt");
+		fGrid >> gridBegin >> gridEnd >> numberOfPartitions >> dischargeRatio;
+		fGrid >> tBegin >> tEnd >> dt;
+	}
+
+	Init(string grid)
+	{
+		ReadingGrid(grid);
+	}
 };
