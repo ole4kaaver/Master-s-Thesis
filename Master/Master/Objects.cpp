0b1f53e48282be37dd94a24058994697d77197e6
Оля Аверкина <olga.averkina.01@mail.ru>
2023-11-13 13:56:17 +0700

    'test2'

--- Master/Master/Objects.cpp
+++ Master/Master/Objects.cpp
@@ -1,1 +1,22 @@
-using namespace std
+#include <vector>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+class Component
+{
+public:
+	int number;
+	double massFraction;
+	double amountOfSubstance;
+};
+
+class Phase : Component
+{
+public:
+	int number;
+	double saturation;
+	double multiplierToPhasePermeability;
+	double viscosity;
+	double density;
+};
