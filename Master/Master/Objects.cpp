f4a7f9e447860f9902ceab2de0e0e35c0ce8701a
Оля Аверкина <olga.averkina.01@mail.ru>
2023-11-13 15:35:48 +0700

    'AddClassesAndMethodsinObjects.cpp'

--- Master/Master/Objects.cpp
+++ Master/Master/Objects.cpp
@@ -10,9 +10,16 @@ public:
 	int number;
 	double massFraction;
 	double amountOfSubstance;
+
+	Component(int num, double massFrac, double amountOfSub)
+	{
+		number = num;
+		massFraction = massFrac;
+		amountOfSubstance = amountOfSub;
+	}
 };
 
-class Phase : Component
+class Phase
 {
 public:
 	int number;
@@ -20,23 +27,42 @@ public:
 	double multiplierToPhasePermeability;
 	double viscosity;
 	double density;
-	list <Component> components;
+
+	Phase(int num, double satur, double multiplierToPhasePerm, double viscos, double den)
+	{
+		number = num;
+		saturation = satur;
+		multiplierToPhasePermeability = multiplierToPhasePerm;
+		viscosity = viscos;
+		density = den;
+	}
 };
 
-class FiniteElement : Phase
+class FiniteElement
 {
 public:
 	int number;
 	double xBegin, xEnd;
 	list <Phase> phases;
+	list <Component> components;
+
+	FiniteElement(int num, double x1, double x2, list <Phase> phasesCur, list <Component> componentsCur)
+	{
+		number = num;
+		xBegin = x1;
+		xEnd = x2;
+		phases = phasesCur;
+		components = componentsCur;
+	}
 };
 
-class Init : FiniteElement
+class Init
 {
 public:
 	double gridBegin, gridEnd, dischargeRatio;
 	int numberOfPartitions;
 	double tBegin, tEnd, dt;
+	vector <FiniteElement> elements;
 
 	void ReadingGrid(string grid)
 	{
@@ -46,8 +72,37 @@ public:
 		fGrid >> tBegin >> tEnd >> dt;
 	}
 
-	Init(string grid)
+	void BuildingGrid(vector <FiniteElement> elements)
+	{
+		double h;
+		double length = gridEnd - gridBegin;
+		if (dischargeRatio != 1)
+		{
+			if (dischargeRatio < 0)
+				dischargeRatio = 1 / abs(dischargeRatio);
+			h = length * (1 - dischargeRatio) / (1 - pow(dischargeRatio, numberOfPartitions));
+		}
+		else h = length / numberOfPartitions;
+	}
+
+	void ReadingParameters(string parameters)
+	{
+		ifstream fParam;
+		fParam.open(parameters + ".txt");
+		int count = 0;
+		fParam >> count;
+		for (int i = 0; i < count; i++)
+		{
+			
+		}
+
+
+	}
+
+	Init(string grid, string parameters)
 	{
 		ReadingGrid(grid);
+		elements.resize(numberOfPartitions);
+		BuildingGrid(elements);
 	}
 };
